f5ddb9cf309dc2c47bea944514ea60c7d99a3df0
jv_h2d <jorge.valdez@h2d.app>
2022-06-13 11:53:08 -0500

    lazy load setted up

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+      {
+        path: 'characters',
+        loadChildren: ()=> import('./modules/characters/characters.module').then(m => m.CharactersModule)
+      },
+      {
+        path: 'episodes',
+        loadChildren: ()=> import('./modules/episodes/episodes.module').then(m => m.EpisodesModule)
+      },
+      {
+        path: 'locations',
+        loadChildren: ()=> import('./modules/locations/locations.module').then(m => m.LocationsModule)
+      },
+      {path: '**', redirectTo: 'characters' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
